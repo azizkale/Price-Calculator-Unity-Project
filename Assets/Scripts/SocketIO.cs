a4a59fe512c2fc0196a13fa456253f695a66208e
azizkale <azizkale@hotmail.com>
2022-01-09 09:28:08 +0100

    websocket added

--- Assets/Scripts/SocketIO.cs
+++ Assets/Scripts/SocketIO.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using NativeWebSocket;
 public class SocketIO : MonoBehaviour
 {
+
+    WebSocket websocket;
+
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        
+        websocket = new WebSocket("ws://localhost:5000");
+
+
+        websocket.OnMessage += Websocket_OnMessage1;
+
+
+        // waiting for messages
+        await websocket.Connect();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Websocket_OnMessage1(byte[] data)
     {
-        
+        Debug.Log("coming message: ");
+        var message = System.Text.Encoding.UTF8.GetString(data);
+        Debug.Log("coming message: " + message);
     }
+
+   
+
+    public async void SendWebSocketMessage()
+    {
+        if (websocket.State == WebSocketState.Open)
+        {
+            //// Sending bytes
+            //await websocket.Send(new byte[] { 10, 20, 30 });
+
+            // Sending plain text
+            await websocket.SendText("aziz");
+            
+        }
+    }
+
+    
+
 }
